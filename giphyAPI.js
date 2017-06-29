9821be73666f279ec04fc0fb28ce59e5703acf55
Jake Hamel <jakehamelhs@gmail.com>
2017-06-28 23:00:13 -0500

    Simple updates

--- giphyAPI.js
+++ giphyAPI.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
-	q = "hearthstone"; // search query
+	var q = "I love you"; // search query
 
-	request = new XMLHttpRequest;
+	var request = new XMLHttpRequest;
 	request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+q, true);
 
 	request.onload = function() {
 		if (request.status >= 200 && request.status < 400){
-			data = JSON.parse(request.responseText).data.image_url;
+			var data = JSON.parse(request.responseText).data.image_url;
 			console.log(data);
 			document.getElementById("giphyme").innerHTML = '<img src = "'+data+'"  title="GIF via Giphy">';
 		} else {
